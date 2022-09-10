485fd16231cd961a087cb9ee7819a1abd1adb8b4
Bekket <jackatwork@yandex.ru>
2022-09-10 03:07:02 +0300

    connecting to contract and subscribing to events

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 
 	passport "github.com/MoonSHRD/IKY-telegram-bot/artifacts/TGPassport"
 	//passport "IKY-telegram-bot/artifacts/TGPassport"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 
-	//	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/event"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -50,9 +55,19 @@ var msgTemplates = make (map[string] string)
 var baseURL = "http://localhost:3000/"
 var tg_id_query = "?user_tg_id="
 
+var myenv map[string]string
+
+const envLoc = ".env"
+
+
+
 
 func main() {
 
+
+	loadEnv()
+	ctx := context.Background()
+	pk := myenv["PK"] // load private key from env
 
 	msgTemplates["hello"] = "Hey, this bot is attaching personal wallets to telegram user & collective wallets to chat id"
 	msgTemplates["case0"] = "Go to link and attach your tg_id to your metamask wallet"
@@ -63,6 +78,63 @@ func main() {
 		log.Panic(err)
 	}
 
+		// Connecting to network
+	//  client, err := ethclient.Dial(os.Getenv("GATEWAY"))	// for global env config
+	client, err := ethclient.Dial(myenv["GATEWAY"]) // load from local .env file
+	if err != nil {
+		log.Fatalf("could not connect to Ethereum gateway: %v\n", err)
+	}
+	defer client.Close()
+
+	// setting up private key in proper format
+	privateKey, err := crypto.HexToECDSA(pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Creating an auth transactor
+	auth := bind.NewKeyedTransactor(privateKey)
+
+	// check calls
+	// check balance
+	accountAddress := common.HexToAddress("0x16d97A46030C5D3D705bca45439e48529997D8b2")
+	balance, _ := client.BalanceAt(ctx, accountAddress, nil) //our balance
+	fmt.Printf("Balance of the validator bot: %d\n", balance)
+
+	// Setting up Passport Contract
+	passportCenter, err := passport.NewPassport(common.HexToAddress("0xef2DCEA186FA90FcD0c44C3caaE81E4586eE0685"), client)
+		if err != nil {
+			log.Fatalf("Failed to instantiate a TGPassport contract: %v", err)
+		}
+
+		// Wrap the Passport contract instance into a session
+		session := &passport.PassportSession{
+			Contract: passportCenter,
+			CallOpts: bind.CallOpts{
+				Pending: true,
+				From:    auth.From,
+				Context: context.Background(),
+			},
+			TransactOpts: bind.TransactOpts{
+				From:     auth.From,
+				Signer:   auth.Signer,
+				GasLimit: 0,   // 0 automatically estimates gas limit
+				GasPrice: nil, // nil automatically suggests gas price
+				Context:  context.Background(),
+			},
+		}
+	
+	log.Printf("session with passport centr initialized")
+
+	/*
+	   Events
+	*/
+
+	// Check retriving events of CashOut request
+
+	var ch = make(chan *passport.PassportPassportApplied)
+	subscription, err := SubscribeForApplications(session,ch)
+	
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
@@ -120,6 +192,13 @@ func main() {
 		}
 	}
 }
+
+func loadEnv() {
+	var err error
+	if myenv, err = godotenv.Read(envLoc); err != nil {
+		log.Printf("could not load env from %s: %v", envLoc, err)
+	}}
+
 
 
 // subscribing for Applications events
